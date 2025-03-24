685432d8f3a7902e7378f7bca24d209b36739840
Anton Kiosia <kesya273@gmail.com>
2025-03-24 15:44:11 +0500

    finish

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const array = "({[]})";
+const array = "[({[{()}]})]";
+
+function check(array) {
+    const result1 = array.split("{");
+    const result2 = array.split("}");
+    const result3 = array.split("(");
+    const result4 = array.split(")");
+    const result5 = array.split("[");
+    const result6 = array.split("]");
+    return result1[0].length === result2[result2.length - 1].length && result3[0].length === result4[result4.length - 1].length && result5[0].length === result6[result6.length - 1].length;
+};
+
+console.log(check(array));
